d594584302ae1cbff6d1e6d122edb5965b964e38
Terry Lee <terry.lee@playconcepts.co.uk>
2021-05-24 11:59:19 +0100

    Full PDA token validation implemented

--- app/core/pda_auth.py
+++ app/core/pda_auth.py
@@ -4,6 +4,7 @@ from fastapi.security import OAuth2PasswordBearer, OAuth2PasswordRequestForm
 from .auth import Token
 import requests
 import jwt
+from jwt.exceptions import ExpiredSignatureError, InvalidSignatureError
 import json
 import base64
 import binascii
@@ -23,6 +24,8 @@ def http_exception(detail: str):
 
 invalid_application = http_exception("Credentials are not for this application")
 credentials_exception = http_exception("Credentials could not be validated.")
+expired_signature_exception = http_exception("Credentials have expired.")
+invalid_signature_exception = http_exception("Credentials are invalid.")
 
 
 async def get_current_pda_user(token: str = Depends(oauth2_scheme)):
@@ -35,9 +38,15 @@ async def get_current_pda_user(token: str = Depends(oauth2_scheme)):
 def validate(token: str) -> [Dict[str, Any], str]:
     pda_url = _get_pda_url(token)
     pda_public_key = _get_pda_public_key(pda_url)
-    decoded = jwt.decode(token, pda_public_key,
-                         options={"verify_signature": False, "verify_exp": True},
-                         algorithms=["RS256"])
+    try:
+        decoded = jwt.decode(token, pda_public_key,
+                             options={"verify_signature": True, "verify_exp": True},
+                             algorithms=["RS256"])
+    except ExpiredSignatureError:
+        raise expired_signature_exception
+    except InvalidSignatureError:
+        raise invalid_signature_exception
+
     if decoded['application'] != app_config.APPLICATION_ID:
         raise invalid_application
     return decoded, token
