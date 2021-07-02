c8ad7e876fc6af86d68e8679b13cd8a58a26efe0
Terry Lee <terry.lee@playconcepts.co.uk>
2021-07-02 16:50:31 +0100

    Auth Plugin installation

---
+++ app/db/migrations/versions/f7a36107bdcd_create_user_tables.py
@@ -0,0 +1,38 @@
+"""create_user_tables
+Revision ID: f7a36107bdcd
+Revises: 7e7fad16535e
+Create Date: 2021-07-02 12:34:55.254359
+"""
+from alembic import op
+import sqlalchemy as sa
+from sqlalchemy.dialects.postgresql import UUID
+import uuid
+
+
+# revision identifiers, used by Alembic
+revision = 'f7a36107bdcd'
+down_revision = '7e7fad16535e'
+branch_labels = None
+depends_on = None
+
+
+# https://frankie567.github.io/fastapi-users/configuration/model.html
+def create_users_table() -> None:
+    op.create_table(
+        "users",
+        sa.Column("id", UUID(as_uuid=True), primary_key=True, default=uuid.uuid4),
+        sa.Column("email", sa.VARCHAR(255), nullable=False),
+        sa.Column("hashed_password", sa.VARCHAR(4096), nullable=False),
+        sa.Column("is_active", sa.BOOLEAN, nullable=False),
+        sa.Column("is_superuser", sa.BOOLEAN, nullable=False),
+        sa.Column("is_verified", sa.BOOLEAN, nullable=False),
+        sa.Column("password_change_token", sa.VARCHAR(128), nullable=True)
+    )
+
+
+def upgrade() -> None:
+    pass
+
+
+def downgrade() -> None:
+    op.drop_table("users")
--- app/main.py
+++ app/main.py
@@ -1,8 +1,13 @@
 from fastapi import FastAPI
 from starlette.middleware.cors import CORSMiddleware
 
+from app.apis.dependencies.database import get_database
+from app.core.config import config
+from app.models.user import User, UserCreate, UserUpdate, UserDB
 from app.routes import crm_route, customer_route
 from app.core import auth, pda_auth, tasks
+
+from fastapi import Depends
 
 app = FastAPI(title="c2d CRM", version="0.5.0-20210517")
 
@@ -22,3 +27,62 @@ app.include_router(auth.router)
 app.include_router(pda_auth.router)
 app.include_router(customer_route.router)
 app.include_router(crm_route.router)
+
+
+from fastapi_users.authentication import JWTAuthentication
+
+SECRET = config.SECRET_KEY
+auth_backends = []
+jwt_authentication = JWTAuthentication(secret=SECRET, lifetime_seconds=3600, tokenUrl="auth/jwt/login")
+auth_backends.append(jwt_authentication)
+
+
+from fastapi_users import FastAPIUsers
+from fastapi_users.db import SQLAlchemyBaseUserTable, SQLAlchemyUserDatabase
+from sqlalchemy.ext.declarative import DeclarativeMeta, declarative_base
+
+Base: DeclarativeMeta = declarative_base()
+
+
+class UserTable(Base, SQLAlchemyBaseUserTable):
+    pass
+
+
+users = UserTable.__table__
+user_db = SQLAlchemyUserDatabase(UserDB, Depends(get_database), users)
+
+fastapi_users = FastAPIUsers(
+    user_db,
+    auth_backends,
+    User,
+    UserCreate,
+    UserUpdate,
+    UserDB,
+)
+
+app.include_router(
+    fastapi_users.get_auth_router(jwt_authentication),
+    prefix="/auth/jwt",
+    tags=["auth"],
+)
+app.include_router(
+    fastapi_users.get_register_router(),
+    prefix="/auth",
+    tags=["auth"],
+)
+app.include_router(
+    fastapi_users.get_reset_password_router(SECRET),
+    prefix="/auth",
+    tags=["auth"],
+)
+app.include_router(
+    fastapi_users.get_users_router(),
+    prefix="/users",
+    tags=["users"],
+)
+app.include_router(
+    fastapi_users.get_verify_router(SECRET),
+    prefix="/auth",
+    tags=["auth"],
+)
+
