ac7a747a6991da772e1da301711f5d25301860b4
Terry Lee <terry.lee@playconcepts.co.uk>
2021-07-21 12:33:43 +0100

    Move MAILER_FROM to env

--- app/apis/utils/emailer.py
+++ app/apis/utils/emailer.py
@@ -3,25 +3,27 @@ from typing import Any, Dict, List
 
 import boto3
 
+from app.core.config import config as app_config
+
 session = boto3.Session()
 ses = session.client("ses")
 
 
 def send_bulk_templated_email(destinations: List[Any],
                               template: str, template_defaults: str,
-                              source: str = 'systems@dataswift.dev'):
+                              source: str = app_config.MAILER_FROM):
     return ses.send_bulk_templated_email(
         Source=source,
         Template=template,
         DefaultTemplateData=template_defaults,
-        Destinations=destinations
+        Destinations=destinations,
     )
 
 
 def send_templated_email(to: str,
                          template: str,
                          template_data: Dict[str, Any],
-                         source: str = 'systems@dataswift.dev'):
+                         source: str = app_config.MAILER_FROM):
     return ses.send_templated_email(
         Source=source,
         Template=template,
@@ -30,6 +32,5 @@ def send_templated_email(to: str,
             'ToAddresses': [
                 to,
             ],
-        }
+        },
     )
-
