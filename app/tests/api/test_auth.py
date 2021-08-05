22bd142c4c8d0876377368676ad9ac7b8ad99aad
Terry Lee <terry.lee@playconcepts.co.uk>
2021-08-05 12:29:57 +0100

    Split the Auth test file between FastAPI's and Custom

--- app/tests/api/test_auth.py
+++ app/tests/api/test_auth.py
@@ -4,7 +4,7 @@ This file tests the api path /api/auth/*
 Authentication Routes are mostly provided for by FastAPIUsers. There is no need to write tests for them, since they
 have included tests. We only need to check that they are installed at the correct routes
 
-The exception is /api/auth/create-password. This is a custom endpoint, which must be tested
+The exception is /api/auth/create-password. This is a custom endpoint, which is in a separate test file
 """
 import pytest
 from fastapi import FastAPI
@@ -52,21 +52,3 @@ class TestAuthRoutes:
     ) -> None:
         res = await client.post("/api/auth/reset-password", json={})
         assert res.status_code == HTTP_422_UNPROCESSABLE_ENTITY
-
-
-class TestAuthCreatePasswordRoutes:
-    @pytest.mark.asyncio
-    async def test_create_password_route_exist(
-        self, app: FastAPI, client: AsyncClient
-    ) -> None:
-        res = await client.post(app.url_path_for("auth:create-password"), json={})
-        assert res.status_code != HTTP_404_NOT_FOUND
-
-    @pytest.mark.asyncio
-    async def test_create_password_input_raises_error(
-        self, app: FastAPI, client: AsyncClient
-    ) -> None:
-        res = await client.post(app.url_path_for("auth:create-password"), json={})
-        assert res.status_code == HTTP_422_UNPROCESSABLE_ENTITY
-
-    # TODO Test the Actual Create Password API
