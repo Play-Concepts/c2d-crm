d27f920cac86c9e4b82a5368897582ce52bbe28b
Terry Lee <terry.lee@playconcepts.co.uk>
2021-08-18 15:37:17 +0100

    test demo data

---
+++ app/tests/data/generate.py
@@ -0,0 +1,3 @@
+for i in range(1, 101):
+    print(f"Jason,Shong{i},{i} Main Street,Elyria,jason.shong+{i}@dataswift.io")
+    print(f"James,Kingston{i},{i} Front Street,Elyria,james.kingston+{i}@dataswift.io")
