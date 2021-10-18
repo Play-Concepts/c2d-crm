b7e0b234d4e8dd51717e71ef14c5d70b9553f23d
Terry Lee <terry.lee@playconcepts.co.uk>
2021-10-18 20:05:49 +0100

    Version up

--- app/main.py
+++ app/main.py
@@ -16,7 +16,7 @@ sentry_sdk.init(dsn=app_config.SENTRY_DSN)
 
 app = FastAPI(
     title="Data Passport API",
-    version="1.0.2-RC-20211018",
+    version="1.0.2-RC1-20211018",
     docs_url="/api/docs",
     redoc_url="/api/redoc",
     openapi_url="/api/openapi.json",
